41e626134093884c5272a2de8611b6513c54dac4
Timo Wang <timowang1991@gmail.com>
2023-10-15 15:39:03 +0800

    use ViewChild (ElementRef)

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -10,8 +10,11 @@ export class CockpitComponent implements OnInit {
   serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
   @Output()
   blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
-  newServerName = '';
-  newServerContent = '';
+  // newServerName = '';
+  // newServerContent = '';
+
+  @ViewChild('serverContentInput', { static: true })
+  serverContentInput: ElementRef;
 
   constructor() { }
 
@@ -27,14 +30,20 @@ export class CockpitComponent implements OnInit {
   onAddServer(nameInput: HTMLInputElement) {
     this.serverCreated.emit({
       serverName: nameInput.value,
-      serverContent: this.newServerContent,
+      serverContent: this.serverContentInput.nativeElement.value,
     });
   }
 
+  // onAddBlueprint(nameInput: HTMLInputElement) {
+  //   this.blueprintCreated.emit({
+  //     serverName: nameInput.value,
+  //     serverContent: this.newServerContent,
+  //   });
+  // }
   onAddBlueprint(nameInput: HTMLInputElement) {
     this.blueprintCreated.emit({
       serverName: nameInput.value,
-      serverContent: this.newServerContent,
+      serverContent: this.serverContentInput.nativeElement.value,
     });
   }
 }
